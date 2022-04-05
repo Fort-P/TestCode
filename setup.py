01472560cefd7092679fa6a61bd02cd654354196
Fort-P <74835871+Fort-P@users.noreply.github.com>
2022-04-05 16:37:23 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='codetesting',
-    packages=find_packages(include=['testCode']),
+    name='functiontester',
+    packages='testcode',
     version='0.1.0',
+    url='https://github.com/Fort-P/functiontester',
     description='A python library that allows you to easily test functions in your code',
-    author='Me',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Fort-P',
     license='MIT',
     install_requires=[],
 )
